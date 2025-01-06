a8bb64a4eecdbecb2427deef183831de41f269db
Rafael Madeira <rafaelsmadeira@gmail.com>
2025-01-06 18:02:37 -0300

    Disable action cable

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Napp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Disable Action Cable
+    config.action_cable.mount_path = nil
   end
 end
